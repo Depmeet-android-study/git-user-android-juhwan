4858e2a11dfd234da9fcf36c9a05284d9909da3e
juhwankim-dev <juhwan.dev@gmail.com>
2022-11-06 00:00:01 +0900

    init: 프로젝트 생성

---
+++ app/src/main/java/com/depromeet/git_user_android_juhwan/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.depromeet.git_user_android_juhwan
+
+import android.app.Application
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ domain/src/main/java/com/depromeet/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.depromeet.domain
+
+class MyClass {
+}
