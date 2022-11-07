e7ab0cede28ad90367a30332e1d6f7e64c5afd1d
juhwankim-dev <juhwan.dev@gmail.com>
2022-11-08 00:32:22 +0900

    feat: Jetpack Navigation 적용

--- presentation/home/src/main/java/com/depromeet/home/MainActivity.kt
+++ presentation/home/src/main/java/com/depromeet/home/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.depromeet.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.depromeet.core.BaseActivity
+import com.depromeet.home.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_example) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bnvExample, navController)
     }
 }
